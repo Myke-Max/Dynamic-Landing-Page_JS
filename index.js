1973141dfe1c5aa7bc32792551e9b71e2287c27c
Mike-Max <scartman1992@gmail.com>
2021-06-09 21:31:50 +0100

    done =)

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ const refs = {
     focus: document.querySelector('#focus'),
 
 }
-console.log(refs.focus)
+
+
+refs.name.addEventListener('focus', onNameClear)
+
+
+refs.name.addEventListener('keypress', setName)
+refs.name.addEventListener('blur', setName)
+
+refs.focus.addEventListener('keypress', setFocus)
+refs.focus.addEventListener('blur', setFocus)
+
 
 function showTime() {
     let time = new Date()
@@ -32,37 +42,82 @@ function addZero(n) {
 function onBgImgChange() {
     let today = new Date()
     hour = today.getHours()
+    
     if (hour <= 12) {
         document.body.style.backgroundImage = "url('./img/morning.jpg')"
+         document.body.style.color = '#B0E0E6'
         refs.greeting.textContent = 'Good Morning'
-    } else if (hour <= 18) {
+        refs.time.style.color = '#1E90FF'
+        
+    } else if (hour <= 22) {
         document.body.style.backgroundImage = "url('./img/evening.jpg')"
+        document.body.style.color = '#B0E0E6'
         refs.greeting.textContent = 'Good evening'
+        refs.time.style.color = '#AFEEEE'
+        
     } else {
         document.body.style.backgroundImage = "url('./img/night.jpg')"
         refs.greeting.textContent = 'Good night'
+        document.body.style.color = '#fff'
+        refs.time.style.color = '#C0C0C0'
     }
 }
 
 function getName() {
-    if (localStorage.getItem('name' === null)) {
-        name.textContent = 'Enter your name'
+    if (localStorage.getItem('name') === null) {
+        refs.name.textContent = 'Enter your name'
+       
     } else {
-        name.textContent = localStorage.getItem('name')
+        refs.name.textContent = localStorage.getItem('name')
     }
 }
 
 function setName(e) {
+    console.log(e)
     if (e.type === 'keypress') {
         if (e.which === 13 || e.keyCode === 13) {
             localStorage.setItem('name', e.target.innerText)
+            refs.name.blur()
         }
     } else {
         localStorage.setItem('name', e.target.innerText)
     }
 }
 
+function getFocus() {
+    if (localStorage.getItem('focus') === null) {
+        refs.focus.textContent = 'Enter your focus'
+       
+    } else {
+        refs.focus.textContent = localStorage.getItem('focus')
+    }
+}
+
+function setFocus(e) {
+    console.log(e)
+    if (e.type === "keypress") {
+        if (e.which === 13 || e.keyCode === 13) {
+            localStorage.setItem('focus', e.target.innerText)
+            refs.focus.blur()
+        }
+    } else {
+        localStorage.setItem('focus', e.target.innerText)
+    }
+}
+
+function onNameClear() {
+    if (refs.name.textContent = 'Enter your name') {
+        refs.name.textContent = ''
+    }
+}
+
+
+
+
+
 // Run
 
 showTime()
 onBgImgChange()
+getName()
+getFocus()
