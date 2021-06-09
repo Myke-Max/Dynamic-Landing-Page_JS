9cc40dda9c36cbbb48308a6bf700669953dd5481
Myke-Max <scartman1992@gmail.com>
2021-06-09 03:23:25 +0100

    add some function and img

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const refs = {
     greeting: document.querySelector('#greeting'),
     name: document.querySelector('#name'),
     focus: document.querySelector('#focus'),
+
 }
 console.log(refs.focus)
 
@@ -13,9 +14,9 @@ function showTime() {
     sec = time.getSeconds()
 
 
-    const amPm = hour >= 12 ? 'PM' : 'AM'
+    // const amPm = hour >= 12 ? 'PM' : 'AM'
 
-    hour = hour % 12 || 12;
+    // hour = hour % 12 || 12;
 
 
 
@@ -28,6 +29,40 @@ function addZero(n) {
     return (parseInt(n, 10) < 10 ? '0' : '') + n
 }
 
+function onBgImgChange() {
+    let today = new Date()
+    hour = today.getHours()
+    if (hour <= 12) {
+        document.body.style.backgroundImage = "url('./img/morning.jpg')"
+        refs.greeting.textContent = 'Good Morning'
+    } else if (hour <= 18) {
+        document.body.style.backgroundImage = "url('./img/evening.jpg')"
+        refs.greeting.textContent = 'Good evening'
+    } else {
+        document.body.style.backgroundImage = "url('./img/night.jpg')"
+        refs.greeting.textContent = 'Good night'
+    }
+}
+
+function getName() {
+    if (localStorage.getItem('name' === null)) {
+        name.textContent = 'Enter your name'
+    } else {
+        name.textContent = localStorage.getItem('name')
+    }
+}
+
+function setName(e) {
+    if (e.type === 'keypress') {
+        if (e.which === 13 || e.keyCode === 13) {
+            localStorage.setItem('name', e.target.innerText)
+        }
+    } else {
+        localStorage.setItem('name', e.target.innerText)
+    }
+}
+
 // Run
 
 showTime()
+onBgImgChange()
